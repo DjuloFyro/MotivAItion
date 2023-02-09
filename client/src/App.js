b64026f0b929d9957c0d208480cb89079003b966
Julian Gil <juliangil2424@gmail.com>
2023-02-09 15:37:14 +0100

    define app.js element

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React, {useState, useEffect} from "react"
+import Tittleapp from "./components/Titleapp";
+import Description from "./components/Description";
+import Session from "./components/Session";
 
 function App() {
   return (
     <div>
-
+      <Tittleapp/>
+      <Session/>
+      <Description />
     </div>
   )
 }
