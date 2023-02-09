4e3804481245bf508443ef6f00b6ebf34d029233
Julian Gil <juliangil2424@gmail.com>
2023-02-09 15:35:13 +0100

    add api to main page

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from config import Config
+import openai
 
+openai.api_key = Config.SECRET_KEY
 app = Flask(__name__)
 
-@app.route("/members")
-def members():
-    return {"members" : ["Member1"]}
+@app.route("/")
+def index():
+    return
 
 if __name__ == "__main__":
     app.run()
